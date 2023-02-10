e081c7919bdcde0dc8e0196b8754ed1918034c07
Remon Shehata <remon.shehata@dexcom.com>
2023-02-10 23:50:04 +0200

    hookup viewmodel and AddItemFragment

--- app/src/main/java/com/example/shoppinglist/features/additem/AddItemViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/features/additem/AddItemViewModel.kt
@@ -1,8 +1,11 @@
 package com.example.shoppinglist.features.additem
 
-import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.shoppinglist.data.ErrorType
+import com.example.shoppinglist.data.State
 import com.example.shoppinglist.data.local.DuplicateItemException
 import com.example.shoppinglist.data.local.models.ShoppingItemEntity
 import com.example.shoppinglist.data.repos.ShoppingListRepo
@@ -13,13 +16,18 @@ import javax.inject.Inject
 @HiltViewModel
 class AddItemViewModel @Inject constructor(private val repo: ShoppingListRepo) : ViewModel() {
 
-    private val 
-    fun saveShoppingItem(shoppingItemEntity: ShoppingItemEntity){
+    private val addResultMutableLiveData = MutableLiveData<State<Int>>() //TODO: pass nothing
+
+    val addResultLiveData: LiveData<State<Int>> = addResultMutableLiveData
+
+    @Suppress("SwallowedException")
+    fun saveShoppingItem(shoppingItemEntity: ShoppingItemEntity) {
         viewModelScope.launch {
             try {
                 repo.saveShoppingItem(shoppingItemEntity)
-            } catch (ex: DuplicateItemException){
-
+                addResultMutableLiveData.postValue(State.Success(1))
+            } catch (ex: DuplicateItemException) {
+                addResultMutableLiveData.postValue(State.Error(ErrorType.DuplicateItem))
             }
         }
     }
