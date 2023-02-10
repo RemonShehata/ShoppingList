7ff235d7f825a3b4b36071e92f2684ffcd0be2cc
Remon Shehata <remon.shehata@dexcom.com>
2023-02-10 23:18:43 +0200

    hook up viewmodel with AddItemFragment

--- app/src/main/java/com/example/shoppinglist/features/additem/AddItemViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/features/additem/AddItemViewModel.kt
@@ -1,11 +1,19 @@
 package com.example.shoppinglist.features.additem
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.shoppinglist.data.local.models.ShoppingItemEntity
 import com.example.shoppinglist.data.repos.ShoppingListRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class AddItemViewModel @Inject constructor(repo: ShoppingListRepo) : ViewModel() {
+class AddItemViewModel @Inject constructor(private val repo: ShoppingListRepo) : ViewModel() {
 
+    fun saveShoppingItem(shoppingItemEntity: ShoppingItemEntity){
+        viewModelScope.launch {
+            val result = repo.saveShoppingItem(shoppingItemEntity)
+        }
+    }
 }
