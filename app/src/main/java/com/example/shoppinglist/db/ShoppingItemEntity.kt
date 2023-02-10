66892fda78538668ee393dd95b734800b22720fd
Remon Shehata <remon.shehata@dexcom.com>
2023-02-10 21:41:46 +0200

    create a shopping item object from UI

--- app/src/main/java/com/example/shoppinglist/db/ShoppingItemEntity.kt
+++ app/src/main/java/com/example/shoppinglist/db/ShoppingItemEntity.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 @Entity
 data class ShoppingItemEntity(
     @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id") val id: Int,
+    @ColumnInfo(name = "id") val id: Int = 0,
     @ColumnInfo(name = "name")val name: String,
     @ColumnInfo(name = "quantity")val quantity: Int,
     @ColumnInfo(name = "description") val description: String
