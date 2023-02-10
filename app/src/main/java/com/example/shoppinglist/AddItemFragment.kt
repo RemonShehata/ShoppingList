78d1a24b83155fba8ca60b1553ddbb72cadb296d
Remon Shehata <remongerges94@gmail.com>
2023-02-10 19:20:00 +0200

    add addItemFragment

---
+++ app/src/main/java/com/example/shoppinglist/AddItemFragment.kt
@@ -0,0 +1,30 @@
+package com.example.shoppinglist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.shoppinglist.databinding.FragmentAddItemBinding
+import com.example.shoppinglist.databinding.FragmentShoppingListBinding
+
+class AddItemFragment : Fragment() {
+
+    private lateinit var binding: FragmentAddItemBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAddItemBinding.inflate(layoutInflater).apply {
+
+        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
