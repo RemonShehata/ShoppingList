52ea4851ccdff8fdf803e8caa3a3ad0a1f998182
Remon Shehata <remon.shehata@dexcom.com>
2023-02-10 21:20:00 +0200

    add insert and query for shopping list dao

--- app/src/main/java/com/example/shoppinglist/db/ShoppingListDao.kt
+++ app/src/main/java/com/example/shoppinglist/db/ShoppingListDao.kt
@@ -1,9 +1,16 @@
 package com.example.shoppinglist.db
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 
 @Dao
 interface ShoppingListDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertShoppingItem(shoppingItemEntity: ShoppingItemEntity): Long
 
+    @Query("SELECT * FROM ShoppingItemEntity")
+    suspend fun getShoppingListItemsSync(): List<ShoppingItemEntity>
 }
