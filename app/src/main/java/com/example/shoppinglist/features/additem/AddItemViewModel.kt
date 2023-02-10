13d164c486a2f989d8561aa5c4c0e1f0a28a7c64
Remon Shehata <remongerges94@gmail.com>
2023-02-10 23:29:20 +0200

    create custom exception to handle duplicate item hook it all up with the viewmodel

--- app/src/main/java/com/example/shoppinglist/features/additem/AddItemViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/features/additem/AddItemViewModel.kt
@@ -1,7 +1,9 @@
 package com.example.shoppinglist.features.additem
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.shoppinglist.data.local.DuplicateItemException
 import com.example.shoppinglist.data.local.models.ShoppingItemEntity
 import com.example.shoppinglist.data.repos.ShoppingListRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,9 +13,14 @@ import javax.inject.Inject
 @HiltViewModel
 class AddItemViewModel @Inject constructor(private val repo: ShoppingListRepo) : ViewModel() {
 
+    private val 
     fun saveShoppingItem(shoppingItemEntity: ShoppingItemEntity){
         viewModelScope.launch {
-            val result = repo.saveShoppingItem(shoppingItemEntity)
+            try {
+                repo.saveShoppingItem(shoppingItemEntity)
+            } catch (ex: DuplicateItemException){
+
+            }
         }
     }
 }
