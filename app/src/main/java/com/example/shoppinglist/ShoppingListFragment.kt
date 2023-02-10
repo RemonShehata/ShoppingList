6f78fe2cdad32a438b0a06e37224b32b7980d016
Remon Shehata <remon.shehata@dexcom.com>
2023-02-10 18:59:11 +0200

    add shopping list fragment and nav graph

---
+++ app/src/main/java/com/example/shoppinglist/ShoppingListFragment.kt
@@ -0,0 +1,28 @@
+package com.example.shoppinglist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.shoppinglist.databinding.FragmentShoppingListBinding
+
+class ShoppingListFragment : Fragment() {
+
+    private lateinit var binding: FragmentShoppingListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentShoppingListBinding.inflate(layoutInflater).apply {
+        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
