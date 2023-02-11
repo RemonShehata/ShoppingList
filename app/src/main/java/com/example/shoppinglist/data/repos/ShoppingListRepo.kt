c403113297c001300a662fc26a0cd0b82c2ed7b1
Remon Shehata <remon.shehata@dexcom.com>
2023-02-11 15:28:10 +0200

    make the db return flow of list of items

--- app/src/main/java/com/example/shoppinglist/data/repos/ShoppingListRepo.kt
+++ app/src/main/java/com/example/shoppinglist/data/repos/ShoppingListRepo.kt
@@ -1,8 +1,10 @@
 package com.example.shoppinglist.data.repos
 
 import com.example.shoppinglist.data.local.models.ShoppingItemEntity
+import kotlinx.coroutines.flow.Flow
 
 interface ShoppingListRepo {
     suspend fun saveShoppingItem(item: ShoppingItemEntity): Long
     suspend fun getShoppingList(): List<ShoppingItemEntity>
+    fun getShoppingListFlow(): Flow<List<ShoppingItemEntity>>
 }
