1af45cb0c4dc0487fe39daf0921d4e22720814bc
Remon Shehata <remongerges94@gmail.com>
2023-02-11 18:30:48 +0200

    update state class so that the error is generic just like the data

--- app/src/main/java/com/example/shoppinglist/data/State.kt
+++ app/src/main/java/com/example/shoppinglist/data/State.kt
@@ -1,11 +1,20 @@
 package com.example.shoppinglist.data
 
-sealed class State<out T> {
-    object Loading : State<Nothing>()
-    data class Error(val errorType: ErrorType) : State<Nothing>()
-    data class Success<T>(val data: T) : State<T>()
+sealed class State<out T, out E> {
+    object Loading : State<Nothing, Nothing>()
+    data class Error<E>(val errorType: E) : State<Nothing, E>()
+    data class Success<T>(val data: T) : State<T, Nothing>()
 }
 
-sealed class ErrorType {
-    object DuplicateItem: ErrorType()
+sealed class InsertionError {
+    object DuplicateItem : InsertionError()
+
+}
+
+sealed class UpdateError {
+    object Failure : UpdateError()
+}
+
+sealed class QueryError {
+    object GeneralError : QueryError()
 }
