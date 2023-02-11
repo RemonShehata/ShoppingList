672c2751c118037fb79c2d035e4b4e28d9ebed0e
Remon Shehata <remongerges94@gmail.com>
2023-02-11 18:36:21 +0200

    implement is bought logic use the new error states

--- app/src/main/java/com/example/shoppinglist/data/local/ShoppingListDatabase.kt
+++ app/src/main/java/com/example/shoppinglist/data/local/ShoppingListDatabase.kt
@@ -2,9 +2,9 @@ package com.example.shoppinglist.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.example.shoppinglist.data.local.models.ShoppingItemEntity
+import com.example.shoppinglist.data.local.models.ShoppingEntity
 
-@Database(entities = [ShoppingItemEntity::class], version = DATABASE_VERSION)
+@Database(entities = [ShoppingEntity::class], version = DATABASE_VERSION)
 abstract class ShoppingListDatabase : RoomDatabase() {
 
     abstract fun shoppingListDao(): ShoppingListDao
