7a146a70559963d5393aaad1e163534ef3909e97
Remon Shehata <remon.shehata@dexcom.com>
2023-02-11 19:37:14 +0200

    show only not bought items

--- app/src/main/java/com/example/shoppinglist/data/local/ShoppingListDao.kt
+++ app/src/main/java/com/example/shoppinglist/data/local/ShoppingListDao.kt
@@ -19,6 +19,9 @@ interface ShoppingListDao {
     @Query("SELECT * FROM shopping_entity")
      fun getShoppingListItemsFlow(): Flow<List<ShoppingEntity>>
 
+    @Query("SELECT * FROM shopping_entity WHERE is_bought = 0")
+    fun getShoppingListNotBoughtItemsFlow(): Flow<List<ShoppingEntity>>
+
     @Query("UPDATE shopping_entity SET is_bought = :isBought WHERE name = :itemName")
      suspend fun updateBoughtStatus(itemName: String, isBought: Boolean): Int
 }
