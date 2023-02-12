56a2fa8410cc7ffa58a4b7ddf0fab54394f41b0c
Remon Shehata <remon.shehata@dexcom.com>
2023-02-12 14:29:40 +0200

    update PreferencesManager to have filter for both bought and not bought provide PreferencesManager in common module

--- app/src/main/java/com/example/shoppinglist/di/CommonModule.kt
+++ app/src/main/java/com/example/shoppinglist/di/CommonModule.kt
@@ -1,11 +1,14 @@
 package com.example.shoppinglist.di
 
+import android.content.Context
+import com.example.shoppinglist.data.PreferencesManager
 import com.example.shoppinglist.data.local.ShoppingListDao
 import com.example.shoppinglist.data.repos.ShoppingListRepo
 import com.example.shoppinglist.data.repos.ShoppingListRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -19,4 +22,9 @@ object CommonModule {
         return ShoppingListRepository(dao)
     }
 
+    @Singleton
+    @Provides
+    fun providePreferencesManager(@ApplicationContext context: Context): PreferencesManager {
+        return PreferencesManager(context)
+    }
 }
