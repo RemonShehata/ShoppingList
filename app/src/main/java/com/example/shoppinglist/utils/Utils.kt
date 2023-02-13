ba10b9196a3bbc9ec9550a05b6b30f13d6decfd4
Remon Shehata <remon.shehata@dexcom.com>
2023-02-13 18:10:11 +0200

    add UI for menu items

--- app/src/main/java/com/example/shoppinglist/utils/Utils.kt
+++ app/src/main/java/com/example/shoppinglist/utils/Utils.kt
@@ -2,6 +2,7 @@ package com.example.shoppinglist.utils
 
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import com.google.android.material.textfield.TextInputEditText
 
@@ -11,6 +12,19 @@ fun TextInputEditText.getInputEditTextString() {
 
 fun TextInputEditText.getInputEditTextInt() {
     this.text.toString().toInt()
+}
+
+inline fun SearchView.onQueryTextChanged(crossinline listener: (String) -> Unit) {
+    this.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        override fun onQueryTextSubmit(query: String?): Boolean {
+            return true
+        }
+
+        override fun onQueryTextChange(newText: String?): Boolean {
+            listener(newText.orEmpty())
+            return true
+        }
+    })
 }
 
 fun Fragment.showToast(message: String) {
